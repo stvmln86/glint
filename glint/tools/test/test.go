2edba24be57c02d59c2371d8b3cb929052eadba9
Stephen Malone <mail@wirehaiku.org>
2025-05-04 11:05:26 +1000

    Added tools/test package.

---
+++ glint/tools/test/test.go
@@ -0,0 +1,59 @@
+// Package test implements unit testing functions and data.
+package test
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// MockFiles is a base:body map of mock files for unit testing.
+var MockFiles = map[string]string{
+	"alpha.extn":    "Alpha.\n",
+	"bravo.extn":    "Bravo.\n",
+	"charlie.trash": "Charlie.\n",
+}
+
+// AssertDire asserts a directory's files are equal to a base:body map.
+func AssertDire(t *testing.T, dire string, files map[string]string) {
+	for base, body := range files {
+		orig := filepath.Join(dire, base)
+		AssertFile(t, orig, body)
+	}
+}
+
+// AssertErr asserts an error matches a format string.
+func AssertErr(t *testing.T, err error, form string) {
+	form = strings.ReplaceAll(form, "%q", `".*"`)
+	form = strings.ReplaceAll(form, "%s", `.*`)
+	assert.Regexp(t, form, err.Error())
+}
+
+// AssertFile asserts a file's body is equal to a string.
+func AssertFile(t *testing.T, orig, body string) {
+	bytes, err := os.ReadFile(orig)
+	assert.Equal(t, body, string(bytes))
+	assert.NoError(t, err)
+}
+
+// MockDire returns a temporary directory populated with MockFiles.
+func MockDire(t *testing.T) string {
+	dire := t.TempDir()
+	for base, body := range MockFiles {
+		dest := filepath.Join(dire, base)
+		os.WriteFile(dest, []byte(body), 0777)
+	}
+
+	return dire
+}
+
+// MockFile returns a temporary file populated with a MockFiles entry.
+func MockFile(t *testing.T, base string) string {
+	dire := t.TempDir()
+	dest := filepath.Join(dire, base)
+	os.WriteFile(dest, []byte(MockFiles[base]), 0777)
+	return dest
+}
---
+++ glint/tools/test/test_test.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestAssertDire(t *testing.T) {
+	// setup
+	dire := MockDire(t)
+
+	// success
+	AssertDire(t, dire, MockFiles)
+}
+
+func TestAssertErr(t *testing.T) {
+	// setup
+	err := errors.New(`test "error"`)
+
+	// success
+	AssertErr(t, err, "%s %q")
+}
+
+func TestAssertFile(t *testing.T) {
+	// setup
+	orig := MockFile(t, "alpha.extn")
+
+	// success
+	AssertFile(t, orig, "Alpha.\n")
+}
+
+func TestMockDire(t *testing.T) {
+	// success
+	dire := MockDire(t)
+	AssertDire(t, dire, MockFiles)
+}
+
+func TestMockFile(t *testing.T) {
+	// success
+	orig := MockFile(t, "alpha.extn")
+	AssertFile(t, orig, "Alpha.\n")
+}
