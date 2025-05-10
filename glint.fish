6aa25102f53a2c0750b10b13c2851d29d22c4bac
Stephen Malone <mail@wirehaiku.org>
2025-05-10 12:22:26 +1000

    Cleaned up and simplified code.

--- glint.fish
+++ glint.fish
@@ -2,9 +2,7 @@
 #          glint · minimal note-taking functions in fish · by Stephen Malone          #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                      part one · environment variable functions                      #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# part one: environment variable functions
 
 function _g_evar_dire -d "Echo a resolved GLINT_DIR."
 	echo (path resolve $GLINT_DIR)
@@ -15,12 +13,21 @@ function _g_evar_extn -d "Echo a lowercased no-dot GLINT_EXT."
 	echo (string lower $trim)
 end
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                     part two · file path manipulation functions                     #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# part two: file path manipulation functions
 
-function _g_path_join -d "Echo a joined dire/name.extn path." -a dire name extn
+function _g_path_join -d "Echo a GLINT_DIR/name.GLINT_EXT path." -a name
+	set dire (_g_evar_dire)
+	set extn (_g_evar_extn)
 	echo "$dire/$name.$extn"
+end
+
+function _g_path_list -d "Echo all paths in GLINT_DIR/*.GLINT_EXT."
+	set dire (_g_evar_dire)
+	set extn (_g_evar_extn)
+
+	for path in $dire/*.$extn
+		echo $path
+	end
 end
 
 function _g_path_name -d "Echo a lowercased path name." -a path
@@ -28,19 +35,12 @@ function _g_path_name -d "Echo a lowercased path name." -a path
 	echo (string lower $name)
 end
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                     part three · file system handling functions                     #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# part three: file system handling functions
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                           part four · main glint functions                          #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# part four: main glint functions
 
 function gls -d "List all notes, or notes starting with TEXT." -a text
-	set dire (_g_evar_dire)
-	set extn (_g_evar_extn)
-	
-	for path in $dire/*.$extn
+	for path in (_g_path_list) 
 		set name (_g_path_name $path)
 		set okay (string match --entire "$text*" $name)
 
@@ -50,11 +50,9 @@ function gls -d "List all notes, or notes starting with TEXT." -a text
 	end
 end
 
-function gop -d "Open NOTE in $VISUAL." -a note
-	set dire (_g_evar_dire)
-	set extn (_g_evar_extn)
+function gop -d "Open NOTE in VISUAL editor." -a note
 	set name (_g_path_name $note) 
-	set path (_g_path_join $dire $name $extn)
-	eval "$VISUAL $path"
+	set path (_g_path_join $name)
+	$VISUAL $path
 end
 	
